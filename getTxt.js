19d298ffcda325e1ed663c3d9d7d61d25e211de6
Ja_Hwang <dmagk560@gmail.com>
2020-07-04 19:09:15 +0900

    Second commit

--- getTxt.js
+++ getTxt.js
@@ -3,7 +3,6 @@
 1) 페이지의 문자를 가져와 한 문장씩 나눠서 저장함.
 2) 한문장 출력 후 다음 버튼을 누르면 다음 문장 출력.
 2019.09.18 clear9
-
 2단계
 1) 입력 창을 만들고 입력을 받은 뒤 오타가 있을시 해당 문자 빨간색으로 오타가 없다면 파란색으로 표시
 2) 영문일 경우 WPM 으로 측정 한글일 경우 OO타로 측정.
@@ -28,9 +27,7 @@ var btnNext = document.getElementById('btnNext');
 var typingArea = document.getElementById('typingArea');
 var resetButton;
 var content_Cnt = 0;
-var diablo;
 
-//createReset 이해 잘 안됨
 function createReset()
 {
   resetButton = document.createElement('button');
@@ -67,38 +64,40 @@ function changed_Wrd(size, weight)
 {
   outputTxt.style.fontSize = size;
   outputTxt.style.font = weight;
-  //outputTxt.textContent = script;
 }
 
 function addTag()
 {
+  if(outputTxt.textContent != null)
+  {
+    outputTxt.textContent = null;
+  }
+
   for(var i = 0; i < getString[content_Cnt].length; i++)
   {
     var node = document.createElement('span');
+    node.id = i;
     var txtNode = document.createTextNode(getString[content_Cnt][i]);
     node.appendChild(txtNode);
-    diablo = diablo + node;
+    document.getElementById('outputText').appendChild(node);
   }
-  debugger;
-  document.getElementById('outputText').appendChild(diablo);
 }
 
 function changed_Color(color, num)
 {
-  //console.log(tpField.value.length);
-  outputTxt.textContent[num].style.color = color;
-  // 글자들 태그만 달면 이 코드 쓰면 끝document.getElementById(num).style.color = color;
+  var temp = document.getElementById(num);
+  console.log(num);
+  temp.style.color = color;
 }
-
-//var wrd_Cnt = tpField.value.length;
 
 function startTyping()
 {
   if(content_Cnt >= contentLen)
   {
     buttonOnOff(true); // 더 이상 내용이 없다면 모든 버튼 비활성화
-    changed_Wrd('25px', 'bold', 'Content No more');
+    changed_Wrd('25px', 'bold');
     outputTxt.style.color = 'red';
+    outputTxt.textContent = "No more contents";
     createReset();
     resetButton.addEventListener('click', resetTyping);
   }
@@ -126,9 +125,9 @@ function check_Wrd()
 {
   var count = tpField.value.length;
 
-  if(tpField.value[count-1] == outputTxt.textContent[count-1])
+  if(tpField.value[count] == outputTxt.textContent[count])
   {
-    changed_Color('blue', tpField.value[count-1]);
+    changed_Color('blue', count);
   }
   /*else if(count == 0)
   {
@@ -136,7 +135,7 @@ function check_Wrd()
   }*/
   else
   {
-    changed_Color('red', tpField.value[count-1]);
+    changed_Color('red', count);
   }
 }
 
@@ -152,15 +151,12 @@ btnNext.addEventListener('click', nextContent);
 ///////////큰 퀘스트///////////
 ** outputText가 화면에 나오면 그 길이에 맞추어 input width 변경
 ** 글을 가져와 글자 하나 하나에 전부 span tag 추가
-
 //////////디테일 퀘스트 //////////
 ** clear 버튼과 input 합치기
 ** Get description 버튼 누르면 #typingArea 활성화
 ** 특수문자 제거
-
  ////////// 버  그  //////////
  **한글 타이핑시 시간 안감
-
  -----------------------------------------------------
  /////////// 해결한거 ///////////
  ** 타이핑을 시작하면 시간 측정 시작
